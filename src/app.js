85a92a59eef8a18a5ad688a9ef3bb3b2a456504d
McGorger <764956153@qq.com>
2020-08-12 12:44:12 +0800

    使用chai.spy 监听回调函数

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ new Vue({
     },
 })
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies);
 const expect  = chai.expect;
 // 单元测试
 {
@@ -67,12 +69,10 @@ const expect  = chai.expect;
         }
     });
     vm.$mount();  
+    let spy = chai.spy(function(){})
+    vm.$on('click',spy)
     let  button  =  vm.$el;
-    vm.$on('click',function(){
-       expect(1).to.eq(1);
-    })
-  
     button.click();
-    // expect(order).to.eq('1');
+    expect(spy).to.have.been.called();
   
 }
