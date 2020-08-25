240cd3e460726a9abd66a87ec72255e08733f65b
McGorger <764956153@qq.com>
2020-08-25 19:13:22 +0800

    重构row、col

--- src/row.vue
+++ src/row.vue
@@ -1,31 +1,37 @@
 <template>
- <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
-<slot></slot>
- </div>
+  <div class="row" :style="rowStyle">
+    <slot></slot>
+  </div>
 </template>
 
 <script lang='ts'>
- export default {
-    name:'GuluRow',
-    props:{
-        gutter:{
-            type:[Number,String]
-        }
-    }
-    ,
-    mounted() {
-        
-     this.$children.forEach((vm) => {
+export default {
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [Number, String],
+    },
+  },
+  computed: {
+    rowStyle() {
+      let { gutter } = this;
+      return {
+        marginLeft: -gutter / 2 + "px",
+        marginRight: -gutter / 2 + "px",
+      };
+    },
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
       vm.gutter = this.gutter;
     });
   },
- }
+};
 </script>
 
 <style scoped lang='scss'>
-.row{
-    display: flex;
-    margin: 0 -10px;
+.row {
+  display: flex;
+  margin: 0 -10px;
 }
- 
 </style>
