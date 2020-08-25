07ee7f949b86726729d2e52864147381ed3fae0d
McGorger <764956153@qq.com>
2020-08-25 18:53:55 +0800

    基本实现row、col的功能

--- src/col.vue
+++ src/col.vue
@@ -1,31 +1,48 @@
 
 <template>
- <div class="col" :class="[`col-${span}`]">
- <slot></slot>
- </div>
+  <div
+    class="col"
+    :class="[ span&&`col-${span}`,offset&&`offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
+    <slot></slot>
+  </div>
 </template>
 
 <script>
- export default {
-     name:'GuluCol',
-     props:{
-         span:{type:[Number,String]}
-     }
- }
+export default {
+  name: "GuluCol",
+  props: {
+    span: { type: [Number, String] },
+    offset: {
+      type: [Number, String],
+    },
+  },
+  data() {
+    return {
+      gutter: 0,
+    };
+  },
+};
 </script>
 
 <style scoped lang='scss'>
- .col {
-            height: 100px;
-            background: grey;
-            border: 1px solid red;
-            width: 50%;
-            $class-prefix:col-;
-            @for $n from 1 through 24 {
-                &.#{$class-prefix}#{$n}{
-                    width: ($n/24)*100%;
-                }
-            }
-        }
- 
+.col {
+  height: 100px;
+  background: grey;
+  border: 1px solid red;
+  width: 50%;
+  $class-prefix: col-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
+    }
+  }
+}
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,17 +1,31 @@
 <template>
- <div class="row">
+ <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
 <slot></slot>
  </div>
 </template>
 
 <script lang='ts'>
  export default {
+    name:'GuluRow',
+    props:{
+        gutter:{
+            type:[Number,String]
+        }
+    }
+    ,
+    mounted() {
+        
+     this.$children.forEach((vm) => {
+      vm.gutter = this.gutter;
+    });
+  },
  }
 </script>
 
 <style scoped lang='scss'>
 .row{
     display: flex;
+    margin: 0 -10px;
 }
  
 </style>
